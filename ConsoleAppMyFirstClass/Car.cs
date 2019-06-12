9ab2691711d9dd2c83d33c2bc1d1fde395fb423e
Ulf Bengtsson <ulf.bengtsson@lexicon.se>
2019-06-12 11:42:21 +0200

    Add project files.

---
+++ ConsoleAppMyFirstClass/Car.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppMyFirstClass
+{
+    class Car
+    {
+        static int carsCreated = 0;
+        public static int CarsCreated
+        {
+            get
+            {
+                return carsCreated;
+            }
+
+            private set
+            {
+                carsCreated = value;
+            }
+        }
+
+        //Any method or field will be private by defualt
+        public string brand;
+        public string modelName;
+        public string color;
+        public int year;
+        public int miles;
+        public int speed;
+
+        public Car( string brand,
+                    string modelName, 
+                    string color, 
+                    int year, 
+                    int miles, 
+                    int currentSpeed)
+        {
+            this.brand = brand;
+            this.modelName = modelName;
+            this.color = color;
+            this.year = year;
+            this.miles = miles;
+            speed = currentSpeed;
+
+            CarsCreated++;
+        }
+
+        public override string ToString()
+        {
+            return $"Car:\nBrand - {brand}\nModel - {modelName}\nColor - {color}\nYear - {year}\nMiles - {miles}\nCurrent speed = {speed}";
+        }
+    }
+}
---
+++ ConsoleAppMyFirstClass/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppMyFirstClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = new List<Car>();
+
+            Console.WriteLine("Cars: " + Car.CarsCreated);
+
+            cars.Add(new Car("Audi", "RS 6", "Black", 2019, 0, 0));
+            cars.Add(new Car("Volvo", "740", "Red", 1998, 56_000, 0));
+            cars.Add(new Car("Lambogini", "Diablo", "Yellow", 2020, 0, 0));
+
+            Console.WriteLine("Cars: " + Car.CarsCreated);
+
+            //foreach (Car aCar in cars)
+            //{
+            //    Console.WriteLine(aCar);
+            //}
+
+            int numberA = 37;
+
+            IncrementInt(numberA);
+
+            Console.WriteLine("utside method: " + numberA);
+
+            Car myCar = cars[0];
+
+            Console.WriteLine("Car miles before: " + myCar.miles);
+
+            IncrementObject(myCar);
+
+            Console.WriteLine("Car miles after: " + myCar.miles);
+
+            Console.ReadKey(true);
+        }
+
+        static void IncrementObject(Car car)
+        {
+            car.miles++;
+        }
+
+        static void IncrementInt(int number)
+        {
+            number++;
+            Console.WriteLine("inside method: " + number);
+        }
+    }
+}
